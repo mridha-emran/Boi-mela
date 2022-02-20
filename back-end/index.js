0475b1a8d24f0d35bac463d61e7095bdcab55c42
mridha-emran <bari.lala490@gmail.com>
2022-02-20 12:18:42 +0100

    backend mongodb connect

--- back-end/index.js
+++ back-end/index.js
@@ -1,2 +1,22 @@
-const app = "hello",
-const city ="dhaka"
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dovtenv=require("dotenv");
+
+ dovtenv.config({
+     path:"./config.env"
+ })
+
+const PORT = process.env.PORT || 8000
+
+
+
+mongoose.connect(process.env.DB, {useNewUrlParser: true,})
+.then(() => {
+    console.log("Connected to MongoDB !");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Connected to Server port : ${PORT}`)
+})
