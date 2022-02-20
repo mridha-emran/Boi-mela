d771e5599207f9860d548a97258b4ec157eb1d7f
mridha-emran <bari.lala490@gmail.com>
2022-02-20 22:57:55 +0100

    books add with curd operation

--- back-end/controllers/books.js
+++ back-end/controllers/books.js
@@ -1,20 +1,112 @@
 const Books = require("../models/bookModel");
 
-
 const addBooks = async(req,res)=>{
-    
-  const book = await Books.create(req.body);
-        
-    res.status(201).json({
-        success: true,
-        book,
-    })
+      try{
+            const book = await Books.create(req.body);        
+              res.status(201).json({
+                  success: true,
+                  book,
+              })
+                
+        }
+        catch(err){
+            res.status(500).json(err)
+        }   
+  
 }
 
 
+const getAllbooks  = async (_req,res)=>{
+
+      try{     
+         const book = await Books.find();
+          res.status(200).json({
+            success: true,
+            book,
+          });     
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+ }
+
+ const updateBooks =async (req,res)=>{
+        // console.log(req.params.id)
+      try{
+         let book = await Books.findById(req.params.id);
+            // console.log(book)
+          if (!book) {
+              return  res.status(404).json({
+                      success: false,
+                      message: "book not found",
+                    });   
+              }
+                  // console.log(req.body)
+         book = await Books.findByIdAndUpdate(req.params.id, req.body, {
+                    new: true,
+                    runValidators: true,
+                    useFindAndModify: false,
+                  });
+
+              res.status(200).json({
+                success: true,
+                book,
+              });    
+                    
+      }
+
+    catch(err){
+        res.status(500).json(err)
+    }
+ }
+
+ const deleteBooks =async (req,res)=>{
+    console.log(req.params.id)
+      try{
+         let book = await Books.findById(req.params.id);
+           if (!book) {
+              return  res.status(404).json({
+                      success: false,
+                      message: "book not found",
+                    });   
+              } 
+            await book.remove();
+
+            res.status(200).json({
+              success: true,
+              message: "book Delete Successfully",
+            });
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+ }
+const getSingleBooks  =async (req,res)=>{
+      try{
+      
+          let book = await Books.findById(req.params.id);
+           if (!book) {
+              return  res.status(404).json({
+                      success: false,
+                      message: "book not found",
+                    });   
+              } 
+
+            res.status(200).json({
+              success: true,
+              book
+            });    
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+ }
 
  
  module.exports ={
-   addBooks
-
+   addBooks,
+  getAllbooks,
+  updateBooks,
+  deleteBooks,
+  getSingleBooks
  }
--- back-end/routes/bookRoute.js
+++ back-end/routes/bookRoute.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const  {
- addBooks
+ addBooks,getAllbooks,updateBooks,deleteBooks,getSingleBooks
 } = require("../controllers/books");
 
+
 router.post("/books/new",addBooks);
+router.get("/books",getAllbooks);
+router.put("/books/:id",updateBooks);
+router.delete("/books/:id",deleteBooks);
+router.get("/books/:id",getSingleBooks);
+
 
 
 
